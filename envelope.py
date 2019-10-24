01799765fc9bf6e9c64f42d79963dce72193f2bd
Flora <flora.fu@mail.utoronto.ca>
2019-10-23 21:15:17 -0400

    got stable orbit

--- envelope.py
+++ envelope.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sin, cos, acos
+from math import sqrt, sin, cos, acos, pi
 
 from RK4 import rk4 
 from vecgram import velocity_vec, get_phi
@@ -14,22 +14,52 @@ rIcap_min, rIcap_max = r/(sin(gmm)), r/(1-cos(gmm))
 rDcap_min, rDcap_max = rIcap_min*(vd/vi), rIcap_max*(vd/vi)
 
 def envelope_dx(s):
-	v1, v2 = velocity_vec(s[0], s[1], get_phi(s[0], s[1]))
-	return np.array([v1, v2])
+	vr1, vr2, vtht1, vtht2 = velocity_vec(s[0], s[2], get_phi(s[0], s[2]))
+	print('%.5f, %.5f'%(s[0], s[2]))
+	return np.array([vr1, vtht1, vr2, vtht2])
 
 
-def envelope_barrier(se, dt=0.05):
-	ss = [se]
+def envelope_barrier(r1, r2, tht1=0, dt=0.05):
+	dtht = acos((r1**2 + r2**2 - r**2)/(2*r1*r2))
+	ss = [np.array([r1, tht1, r2, tht1-dtht])]
 	t = 0
-	while t < 5:
-		ss.append(rk4(envelope_dx, ss[-1], dt))
+	while t < 10:
+		s_ = rk4(envelope_dx, ss[-1], dt)
+		ss.append(s_)
+		# print(s_)
 		t += dt
-	return np.asarray(ss)
+	xs = []
+	for s in ss:
+		# pritn(s)
+		xd = s[0]*cos(s[1])
+		yd = s[0]*sin(s[1])
+		xi = s[2]*cos(s[3])
+		yi = s[2]*sin(s[3])
+		x = [xd, yd, xi, yi]
+		xs.append(x)
+		# print(sqrt((xd - xi)**2 + (yd - yi)**2))
+	return np.asarray(xs)
+k = .5
+r1 = k*(rDcap_max - rDcap_min) + rDcap_min
+r2 = k*(rIcap_max - rIcap_min) + rIcap_min
+# print(get_phi(r1, r2))
+traj = envelope_barrier(r1, r2)
 
+circ = []
+for tht in np.linspace(0, 2*pi, 50):
+	x = r2*cos(tht)
+	y = r2*sin(tht)
+	circ.append([x, y])
+circ = np.asarray(circ)
 
+fig, ax = plt.subplots()
 
-
-
-se = np.array([rDcap_min, rIcap_min])
-env_traj = envelope_barrier(se)
-print(env_traj)
+ax.plot(traj[:,0], traj[:,1])
+ax.plot(traj[:,2], traj[:,3])
+for i, x in enumerate(traj):
+	if i%50 == 0:
+		ax.plot([x[0], x[2]], [x[1], x[3]], 'b--')
+ax.plot(circ[:,0], circ[:,1], '--')
+ax.grid()
+ax.axis('equal')
+plt.show()
--- vecgram.py
+++ vecgram.py
@@ -8,17 +8,28 @@ r = Config.CAP_RANGE
 R = Config.TAG_RANGE
 vd = Config.VD
 vi = Config.VI 
+gmm = acos(vd/vi)
+rIcap_min, rIcap_max = r/(sin(gmm)), r/(1-cos(gmm))
+rDcap_min, rDcap_max = rIcap_min*(vd/vi), rIcap_max*(vd/vi)
 
-def velocity_vec(r1, r2, phi):
+def velocity_vec(r1, r2, phi, backward=True):
 	psi = acos(vd/vi*cos(phi))
 	psi = -abs(psi)
 
 	alpha = acos((r**2 + r1**2 - r2**2)/(2*r1*r))
 	beta = pi - acos((r**2 + r2**2 - r1**2)/(2*r2*r))
-	v1 = -vd*cos(alpha + phi)
-	v2 = -vi*cos(beta + psi)
+	if backward:
+		vr1 = vd*cos(alpha + phi)
+		vr2 = vi*cos(beta + psi)
+		vtht1 = vd*sin(alpha + phi)/r1
+		vtht2 = vi*sin(beta + psi)/r2		
+	else:
+		vr1 = -vd*cos(alpha + phi)
+		vr2 = -vi*cos(beta + psi)
+		vtht1 = -vd*sin(alpha + phi)/r1
+		vtht2 = -vi*sin(beta + psi)/r2
 
-	return v1, v2
+	return vr1, vr2, vtht1, vtht2
 
 def get_phi(r1, r2):
 
@@ -34,23 +45,26 @@ def get_phi(r1, r2):
 		return dk**2
 
 	sol = minimize(dk_dphi, -pi/2)
-	return sol.x[0]
+	return sol.x
 
 
+# k1, k2 = 0.5, 0.6
+# r1 = k1*(rDcap_max - rDcap_min) + rDcap_min
+# r2 = k2*(rIcap_max - rIcap_min) + rIcap_min
+r1, r2 = 8.33016, 10.32174
+v1s, v2s = [], []
+n = 50
+for phi in np.linspace(-pi, 0.9*pi, n):
+	s = velocity_vec(r1, r2, phi, backward=False)
+	v1s.append(s[0])
+	v2s.append(s[1])
+so = velocity_vec(r1, r2, get_phi(r1, r2), backward=False)
 
-# r1, r2 = .8*R, 1.*R
-# v1s, v2s = [], []
-# for phi in np.linspace(-pi, 0, 40):
-# 	v1, v2 = velocity_vec(r1, r2, phi)
-# 	v1s.append(v1)
-# 	v2s.append(v2)
-# v1o, v2o = velocity_vec(r1, r2, get_phi(r1, r2))
-
-# print(v1o, v2o)
-
-# fig, ax = plt.subplots()
-# ax.plot(v1s, v2s)
-# ax.plot([1.2*v1o, 0], [1.2*v2o, 0], 'r')
-# ax.grid()
+fig, ax = plt.subplots()
+ax.plot(v1s, v2s)
+ax.plot(v1s[:3], v2s[:3], 'r.')
+ax.plot(v1s[int(n/2-1):int(n/2+2)], v2s[int(n/2-1):int(n/2+2)], 'y.')
+ax.plot([1.2*so[0], 0], [1.2*so[1], 0], 'r')
+ax.grid()
 # ax.axis('equal')
-# plt.show()
+plt.show()
